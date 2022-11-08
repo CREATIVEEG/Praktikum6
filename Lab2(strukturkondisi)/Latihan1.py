d6f19f0d818f94cc8dbd459df3da526fe1c625c4
Rhendydiki <rhendydikin@gmail.com>
2022-11-08 11:55:49 +0700

    File Python

--- Lab2(strukturkondisi)/Latihan1.py
+++ Lab2(strukturkondisi)/Latihan1.py
@@ -1,14 +1,10 @@
 #Rhendy_Diki_Nugraha
-#Menentukan bilangan terbesar dari dua input
 print("Menentukan bilangan terbesar dari dua bilangan")
 
 a = int(input('Masukkan nilai A: '))
 b = int(input('Masukkan nilai B: '))
-c = int(input('Masukkan nilai C: '))
 
-if a > b and a > c:
+if a > b:
   print("A =",a ,"yang terbesar")
-elif b > a and b > c:
+elif b > a:
   print("B =",b, "yang terbesar")
-else :
-  print("C =",c, "yang terbesar")

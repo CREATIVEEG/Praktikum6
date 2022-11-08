ec5fba24d3698426be9c91c57501e297ed19d63d
Rhendydiki <rhendydikin@gmail.com>
2022-11-08 16:27:55 +0700

    File Python

--- Modul2/BilanganTerbesar.py
+++ Modul2/BilanganTerbesar.py
@@ -1,4 +1,6 @@
+#RhendyDikiNugraha
 print("Menentukan bilangan terbesar dari 3 input")
+
 a = int(input('Masukkan bilangan a: '))
 b = int(input('Masukkan bilangan b: '))
 c = int(input('Masukkan bilangan c: '))
